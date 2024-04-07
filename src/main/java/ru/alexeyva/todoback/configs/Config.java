2696761234e8dfcbfcd3b3ed1d24d5fadb828b10
alexey-va1 <freedeeml@gmail.com>
2024-04-08 00:54:14 +0300

    fix redis

--- src/main/java/ru/alexeyva/todoback/configs/Config.java
+++ src/main/java/ru/alexeyva/todoback/configs/Config.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -37,7 +39,9 @@ public class Config {
         return new RestTemplate();
     }
 
+
     @Bean
+    @Profile("redis")
     public RedisTemplate<String, String> redisTemplate(Environment environment){
         try {
             String redisHost = environment.getProperty("REDIS_HOST");
